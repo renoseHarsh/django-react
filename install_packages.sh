e7d0b91d6fcc6c040a907df0649a418e6ee49f89
renoseHarsh <renoseharsh@gmail.com>
2023-09-09 00:27:26 -0500

    Update install.sh

--- install_packages.sh
+++ install_packages.sh
@@ -3,9 +3,3 @@
 # Install Python packages
 pip install django
 pip install djangorestframework
-
-# Install Node.js packages
-npm install @material-ui/core --legacy-peer-deps
-npm install @babel/plugin-proposal-class-properties
-npm install react-router-dom
-npm install @material-ui/icons --legacy-peer-deps
